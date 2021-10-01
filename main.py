8eaf936d03506e661abee51d6608d7206e0f10d2
DELL <adamwawrzyniak5@gmail.com>
2021-10-01 21:41:57 +0200

    Dodanie opcji

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from PIL import Image
 from tqdm import tqdm
 import typer
 
-def make_thumbnails(thumbnail_size:tuple[int, int], source_dir: str,
-                    extension: str):
+def make_thumbnails(
+        thumbnail_width: int = typer.Option(500, help="Szerokość obrazka."),
+        thumbnail_heigh: int = typer.Option(500, help="Wysokość obrazka."),
+        source_dir: str = typer.Option('AfterFormat', help="Ścieżka końcowa."),
+        extension: str = typer.Option('jpg', help='rozszerzenie.')
+):
     # tqdm i ładny paseczek "ulala" :))
     for file in tqdm(glob(f'{source_dir}/*.{extension}')):
         img = Image.open(file)
-        print(img.size)
-        img.thumbnail(thumbnail_size)
-
-
+        img.thumbnail((thumbnail_width, thumbnail_heigh))
         img.save(file)
+
+if __name__ == '__main__':
+    typer.run(make_thumbnails)
