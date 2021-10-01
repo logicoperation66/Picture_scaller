83a385ae1850f2438cea66d7132ca014424414e5
DELL <adamwawrzyniak5@gmail.com>
2021-10-01 21:06:37 +0200

    Refactor do funkcji

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from glob import glob
 from PIL import Image
 from tqdm import tqdm
+import typer
 
-# Przeskalowanie obrazów do formatu 500x500
-thumbnail_size = (500, 500)
-# tqdm i ładny paseczek "ulala" :))
-for file in tqdm(glob('photo/*.jpg')):
-    img = Image.open(file)
-    print(img.size)
-    img.thumbnail(thumbnail_size)
+def make_thumbnails(thumbnail_size:tuple[int, int], source_dir: str,
+                    extension: str):
+    # tqdm i ładny paseczek "ulala" :))
+    for file in tqdm(glob(f'{source_dir}/*.{extension}')):
+        img = Image.open(file)
+        print(img.size)
+        img.thumbnail(thumbnail_size)
 
 
-    img.save(file)
+        img.save(file)
